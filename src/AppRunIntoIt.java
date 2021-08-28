88b057a2972560ddb93b72e8df250f8530c84a86
CarrieKam <36577595+CarrieKam@users.noreply.github.com>
2021-08-28 18:14:50 -0400

    click enter and journal frame open

--- src/AppRunIntoIt.java
+++ src/AppRunIntoIt.java
@@ -3,6 +3,8 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -17,7 +19,7 @@ import javax.swing.border.EmptyBorder;
 public class AppRunIntoIt extends JFrame {
 
 	private JPanel contentPane;
-
+	private Journal journal = new Journal();
 	/**
 	 * Launch the application.
 	 */
@@ -45,59 +47,71 @@ public class AppRunIntoIt extends JFrame {
 		contentPane.setBackground(new Color(255, 234, 236));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton btnEnter = new JButton("");
+		btnEnter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!journal.isVisible()) {
+					journal.setVisible(true);
+					setVisible(false);
+				}
+			}
+		});
+
 		btnEnter.setBounds(700, 520, 300, 100);
-		associerBoutonAvecImage(btnEnter,"enterButton.png");
+		associerBoutonAvecImage(btnEnter, "enterButton.png");
 		contentPane.add(btnEnter);
 		
+
 		JLabel lblIntroPic = new JLabel(new ImageIcon("images/introPic.PNG"));
 		lblIntroPic.setBounds(0, 0, 1232, 696);
 		contentPane.add(lblIntroPic);
-		
+
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension ecranDimension = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(ecranDimension.width / 2 - getSize().width / 2, ecranDimension.height / 2 - getSize().height / 2);
-		
+
 		setTitle("Run Into It");
-		
 
 	}
-	
-		/**
+
+
+	/**
 	 * Associe une image a un bouton en redimensionnant l'image adequatement.
-	 * @param leBouton Le bouton auquel on veut associer l'image.
+	 * 
+	 * @param leBouton     Le bouton auquel on veut associer l'image.
 	 * @param fichierImage L'image qui sera associee au bouton.
 	 * @Caroline Houle
 	 */
 
-	private void associerBoutonAvecImage( JButton leBouton, String fichierImage ) {		
-		Image imgLue=null;
-		java.net.URL urlImage = getClass().getClassLoader().getResource(fichierImage);  
+	private void associerBoutonAvecImage(JButton leBouton, String fichierImage) {
+		Image imgLue = null;
+		java.net.URL urlImage = getClass().getClassLoader().getResource(fichierImage);
 		if (urlImage == null) {
-			JOptionPane.showMessageDialog(null , "Fichier " + fichierImage + " introuvable");
-		} 
-		try {   
-			imgLue = ImageIO.read(urlImage);  
-		} catch (IOException e) {  
-			JOptionPane.showMessageDialog(null , "Erreur pendant la lecture du fichier d'image"); 
+			JOptionPane.showMessageDialog(null, "Fichier " + fichierImage + " introuvable");
+		}
+		try {
+			imgLue = ImageIO.read(urlImage);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Erreur pendant la lecture du fichier d'image");
 		}
 
-		//redimensionner l'image de la meme grandeur que le bouton
-		Image imgRedim = imgLue.getScaledInstance( leBouton.getWidth(),  leBouton.getHeight(), Image.SCALE_SMOOTH);
+		// redimensionner l'image de la meme grandeur que le bouton
+		Image imgRedim = imgLue.getScaledInstance(leBouton.getWidth(), leBouton.getHeight(), Image.SCALE_SMOOTH);
 
-		//au cas ou le fond de l'image serait transparent
+		// au cas ou le fond de l'image serait transparent
 		leBouton.setOpaque(false);
 		leBouton.setContentAreaFilled(false);
 		leBouton.setBorderPainted(false);
 
-		//associer l'image au bouton
+		// associer l'image au bouton
 		leBouton.setText("");
-		leBouton.setIcon( new ImageIcon(imgRedim) );
+		leBouton.setIcon(new ImageIcon(imgRedim));
 
-		//on se debarrasse des images intermediaires
+		// on se debarrasse des images intermediaires
 		imgLue.flush();
 		imgRedim.flush();
 	}
+
 }
