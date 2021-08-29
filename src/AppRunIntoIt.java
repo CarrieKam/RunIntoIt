1b2537cdc70374f1bc311778a62c132ee96df6fe
CarrieKam <36577595+CarrieKam@users.noreply.github.com>
2021-08-28 23:52:59 -0400

    add three templates in journal

--- src/AppRunIntoIt.java
+++ src/AppRunIntoIt.java
@@ -5,7 +5,11 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,8 +23,6 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class Journal extends JFrame {
 
@@ -32,6 +34,24 @@ public class Journal extends JFrame {
 	private String fontName[];
 	private Integer array[];
 	private JScrollPane scrollPane;
+	private ArrayList<JLabel> templateList = new <JLabel>ArrayList();
+	private JLabel lblNoteTaking;
+	private JLabel lblNoteTaking_1;
+	private JLabel lblNoteTakingWLink;
+	private JLabel lblCloud;
+	private JLabel lblCloud_1;
+	private JLabel lblBird;
+	private JLabel lblWritingJournal;
+	private JLabel lblIdea;
+	private JLabel lblRelaxedBird;
+	private JLabel lblBird_1;
+	private JLabel lblDeskLamp;
+	private JLabel lblDogGlasses;
+	private JLabel lblPencil;
+	private JLabel lblSettings;
+	private JLabel lblOutsideBox;
+	private JLabel lblPencilGreen;
+	private JLabel lblPaint;
 
 	/**
 	 * Launch the application.
@@ -78,25 +98,25 @@ public class Journal extends JFrame {
 		}
 
 		comboBoxFont = new JComboBox(array);
-	
+
 		ComboBoxRenderar renderar = new ComboBoxRenderar();
 		comboBoxFont.setRenderer(renderar);
 		getContentPane().add(comboBoxFont);
 
-		comboBoxFont.setBounds(232, 91, 196, 19);
+		comboBoxFont.setBounds(232, 91, 153, 34);
 		contentPane.add(comboBoxFont);
-	comboBoxFont.addActionListener(new ActionListener() {
+		comboBoxFont.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Font font = new Font((String)(comboBoxFont.getSelectedItem()+""), Font.PLAIN, (int) spnSize.getValue());
+				Font font = new Font((String) (comboBoxFont.getSelectedItem() + ""), Font.PLAIN,
+						(int) spnSize.getValue());
 				textArea.setFont(font);
 			}
 		});
-	
-	
+
 		spnSize = new JSpinner();
 
 		spnSize.setModel(new SpinnerNumberModel(new Integer(12), new Integer(1), null, new Integer(1)));
-		spnSize.setBounds(438, 91, 60, 20);
+		spnSize.setBounds(438, 91, 60, 34);
 		contentPane.add(spnSize);
 
 		scrollPane = new JScrollPane();
@@ -106,6 +126,7 @@ public class Journal extends JFrame {
 		textArea = new JTextArea();
 		scrollPane.setViewportView(textArea);
 		textArea.setLineWrap(true);
+
 		spnSize.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
 				Font font = new Font(comboBoxFont.getFont().getName(), Font.PLAIN, (int) spnSize.getValue());
@@ -117,6 +138,132 @@ public class Journal extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension ecranDimension = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(ecranDimension.width / 2 - getSize().width / 2, ecranDimension.height / 2 - getSize().height / 2);
+
+		int randomNumber = (int) (Math.random() * 3);
+
+		switch (randomNumber) {
+		
+		case 0:
+			lblNoteTaking = new JLabel(new ImageIcon("images/note-taking.png"));
+			lblNoteTaking.setBounds(10, 75, 88, 108);
+			contentPane.add(lblNoteTaking);
+			templateList.add(lblNoteTaking);
+
+			lblNoteTakingWLink = new JLabel(new ImageIcon("images/note-taking-w-link.png"));
+			lblNoteTakingWLink.setBounds(1106, 233, 50, 65);
+			contentPane.add(lblNoteTakingWLink);
+			templateList.add(lblNoteTakingWLink);
+
+			lblCloud = new JLabel(new ImageIcon("images/cloud.png"));
+			lblCloud.setBounds(835, 10, 79, 42);
+			contentPane.add(lblCloud);
+			templateList.add(lblCloud);
+
+			lblCloud_1 = new JLabel(new ImageIcon("images/cloud.png"));
+			lblCloud_1.setBounds(319, 10, 79, 42);
+			contentPane.add(lblCloud_1);
+			templateList.add(lblCloud_1);
+
+			lblBird = new JLabel(new ImageIcon("images/flying-bird.png"));
+			lblBird.setBounds(92, 233, 60, 52);
+			contentPane.add(lblBird);
+			templateList.add(lblBird);
+
+			lblWritingJournal = new JLabel(new ImageIcon("images/writing-journal.png"));
+			lblWritingJournal.setBounds(44, 388, 71, 65);
+			contentPane.add(lblWritingJournal);
+			templateList.add(lblWritingJournal);
+
+			lblIdea = new JLabel(new ImageIcon("images/idea.png"));
+			lblIdea.setBounds(1042, 441, 158, 143);
+			contentPane.add(lblIdea);
+			templateList.add(lblIdea);
+
+			lblRelaxedBird = new JLabel(new ImageIcon("images/relaxed-bird.png"));
+			lblRelaxedBird.setBounds(36, 516, 127, 134);
+			contentPane.add(lblRelaxedBird);
+			templateList.add(lblRelaxedBird);
+
+			lblBird_1 = new JLabel(new ImageIcon("images/flying-bird.png"));
+			lblBird_1.setBounds(1065, 39, 60, 52);
+			contentPane.add(lblBird_1);
+			templateList.add(lblBird_1);
+			break;
+
+		case 1:
+
+			lblDeskLamp = new JLabel(new ImageIcon("images/desk-lamp.png"));
+			lblDeskLamp.setBounds(10, 41, 207, 173);
+			contentPane.add(lblDeskLamp);
+
+			lblDogGlasses = new JLabel(new ImageIcon("images/doggieGlassesComputer.jpg"));
+			lblDogGlasses.setBounds(1037, 210, 174, 173);
+			contentPane.add(lblDogGlasses);
+
+			lblPencil = new JLabel(new ImageIcon("images/pencil.png"));
+			lblPencil.setBounds(879, 10, 112, 126);
+			contentPane.add(lblPencil);
+
+			lblSettings = new JLabel(new ImageIcon("images/settings.png"));
+			lblSettings.setBounds(24, 329, 144, 139);
+			contentPane.add(lblSettings);
+
+			lblOutsideBox = new JLabel(new ImageIcon("images/think-outside-the-box.png"));
+			lblOutsideBox.setBounds(1054, 457, 124, 126);
+			contentPane.add(lblOutsideBox);
+
+			lblBird = new JLabel(new ImageIcon("images/flying-bird.png"));
+			lblBird.setBounds(1092, 73, 60, 52);
+			contentPane.add(lblBird);
+
+			lblCloud = new JLabel(new ImageIcon("images/cloud.png"));
+			lblCloud.setBounds(62, 554, 79, 42);
+			contentPane.add(lblCloud);
+
+			lblCloud_1 = new JLabel(new ImageIcon("images/cloud.png"));
+			lblCloud_1.setBounds(89, 554, 79, 42);
+			contentPane.add(lblCloud_1);
+			break;
+
+		case 2:
+
+			contentPane.setBackground(new Color(48, 26, 75));
+
+			lblPencilGreen = new JLabel(new ImageIcon("images/pencil-green.png"));
+			lblPencilGreen.setBounds(861, 10, 89, 115);
+			contentPane.add(lblPencilGreen);
+
+			lblPaint = new JLabel(new ImageIcon("images/paint-palette.png"));
+			lblPaint.setBounds(1041, 165, 153, 121);
+			contentPane.add(lblPaint);
+
+			lblRelaxedBird = new JLabel(new ImageIcon("images/relaxed-bird.png"));
+			lblRelaxedBird.setBounds(42, 82, 127, 134);
+			contentPane.add(lblRelaxedBird);
+
+			lblJournalTitle.setForeground(Color.PINK);
+
+			lblNoteTaking = new JLabel(new ImageIcon("images/note-taking.png"));
+			lblNoteTaking.setBounds(135, 112, 67, 78);
+			contentPane.add(lblNoteTaking);
+
+			lblNoteTakingWLink = new JLabel(new ImageIcon("images/note-taking-w-link.png"));
+			lblNoteTakingWLink.setBounds(1126, 350, 50, 65);
+			contentPane.add(lblNoteTakingWLink);
+
+			lblWritingJournal = new JLabel(new ImageIcon("images/writing-journal.png"));
+			lblWritingJournal.setBounds(1063, 516, 71, 65);
+			contentPane.add(lblWritingJournal);
+
+			lblIdea = new JLabel(new ImageIcon("images/idea.png"));
+			lblIdea.setBounds(63, 514, 158, 143);
+			contentPane.add(lblIdea);
+
+			lblDeskLamp = new JLabel(new ImageIcon("images/desk-lamp.png"));
+			lblDeskLamp.setBounds(-17, 276, 207, 173);
+			contentPane.add(lblDeskLamp);
+			break;
+		}
 
 	}
 
